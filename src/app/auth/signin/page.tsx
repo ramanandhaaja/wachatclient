a77c6c6949b8c9e0b0d841d9e42afb43286d3c6d
ramanandhaaja <thisisnandha@gmail.com>
2025-03-25 15:48:50 +0700

    update suspense

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from 'next'
+import { Suspense } from 'react';
 import SignInForm from "./SignInForm";
 
 export const metadata: Metadata = {
@@ -9,7 +10,9 @@ export const metadata: Metadata = {
 export default function SignInPage() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-      <SignInForm />
+      <Suspense fallback={<div>Loading...</div>}>
+        <SignInForm />
+      </Suspense>
     </div>
   );
 }
