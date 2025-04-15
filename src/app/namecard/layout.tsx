19e3a7af44500fe65143e27ffe75db3d5746e193
ramanandhaaja <thisisnandha@gmail.com>
2025-04-16 02:14:00 +0700

    resolve hidration problem

--- src/app/namecard/layout.tsx
+++ src/app/namecard/layout.tsx
@@ -1,8 +1,5 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "@/app/globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: {
@@ -17,9 +14,5 @@ export default function CardLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+  return <div>{children}</div>;
 }
